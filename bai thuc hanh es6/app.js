80cdf52b12638bcea9fd527aea7d06688b57c5fb
saikiiii549 <koten1234z@gmail.com>
2022-12-11 21:25:03 +0700

    buoi  th 7

---
+++ bai thuc hanh es6/app.js
@@ -0,0 +1,26 @@
+function student([Name], [Age], [address]) {
+    Name ('Dang Anh ngoc')
+    Age ('16')
+    address ('Quangbinh')
+    console.log([Name], [Age], [address])
+}
+
+function smartPhones(phone) {
+    var smartPhones = [ 
+        { name: 'iphone', price: 649 }, 
+        { name: 'Galaxy S6', price: 576 }, 
+        { name: 'Galaxy Note 5', price: 489 } 
+    ];
+    console.log(phone)
+}
+
+function foo(x,y,z) {
+	console.log( x, y, z );
+}
+function foo () {
+	alert(‘Hello’)
+}
+function foo (a,b){
+	let m = a+b*100
+	return m
+}
