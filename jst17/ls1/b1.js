c14a25dbe73d7984effe02f06e0e14e8ec87c171
saikiiii549 <koten1234z@gmail.com>
2023-03-12 21:23:08 +0700

    jst17

---
+++ jst17/ls1/b1.js
@@ -0,0 +1,28 @@
+var old = prompt("Nhập vào tuổi của bạn", 18);
+    alert("Độ tuổi cần tìm: " + result);
+    if (old != null) {
+    }else {
+    alert("Bạn không nhập vào tuổi");
+}
+let student = [
+    {
+        name: "NVA",
+        age: 16,
+        course: ["mindX 01", "mindX 02", "mindX 03"]
+    },
+    {
+        name: "NVB",
+        age: 18,
+        course: ["mindX 01", "mindX 02", "mindX 04"]
+    },
+    {
+        name: "NVC",
+        age: 17,
+        course: ["mindX 01", "mindX 03", "mindX 04"]
+    },
+    {
+        name: "NVD",
+        age: 15,
+        course: ["mindX 01", "mindX 02", "mindX 03"]
+    }
+]
