89aa669a8cf20101f4a6f649ddf1223c42fa9e0c
saikiiii549 <koten1234z@gmail.com>
2023-05-07 21:30:37 +0700

    ls7

--- jst17/ls4/login.js
+++ jst17/ls4/login.js
@@ -63,7 +63,20 @@ class Login {
         
         
         handleSubmit = (e) => {
-        
+            import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      });
         }
         gotosignin = () =>  {
             const signup = new Register();
